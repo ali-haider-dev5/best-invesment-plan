93612fb6b5b39890e8e57d40c171535792773747
ali-haider-dev <ali.haider.dev514@gmail.com>
2025-10-06 21:40:01 +0500

    inter-font-family-added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
       </head>
       {/* Put the font variable on body (or html) — but only once */}
       <body>
